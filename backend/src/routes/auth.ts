c80de3414ab1eebd1147b75ea08ac9168e7ba843
Robert Gevorgyan <robert1999.g@gmail.com>
2024-11-05 23:48:56 +0400

    Strip the unnecessary user info when sending it from backend

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -20,10 +20,20 @@ router.post('/login', async (req: Request, res: Response) => {
     const { data, error } = await supabase.auth.setSession({
         access_token,
         refresh_token
-    })
+    });
 
+    if (!data.user) throw new Error("No user");
+
+    const { id, role } = data.user;
+    const user = {
+      id,
+      role,
+      email,
+      access_token,
+      refresh_token,
+    }
     if (error) throw error;
-    res.status(200).json(data);
+    res.status(200).json(user);
   } catch (error) {
     const err = (error as AuthError);
     res.status(err?.status || 400).json({ error: err?.message });
@@ -39,8 +49,19 @@ router.post('/register', async (req: Request, res: Response) => {
       password,
     })
 
+    if (!data.user || !data.session) throw new Error("Sign up failed");
+
+    const { id, role } = data.user;
+    const { access_token, refresh_token } = data.session;
+    const user = {
+      id,
+      role,
+      email,
+      access_token,
+      refresh_token,
+    }
     if (error) throw error;
-    res.status(201).json(data);
+    res.status(201).json(user);
   } catch (error) {
     const err = (error as AuthError);
     res.status(err?.status || 400).json({ error: err?.message });
