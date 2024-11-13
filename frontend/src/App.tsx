fc0bade9b51ee2ad3b229ded64af2e31697cb87f
Robert Gevorgyan <robert1999.g@gmail.com>
2024-11-13 18:38:15 +0400

    Add single post route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Posts from "./pages/Posts";
+import SinglePost from "./pages/SinglePost";
 import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
@@ -12,7 +13,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<Posts />} />
-          <Route path="/dashboard" element={<Posts />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/post/:id" element={<SinglePost />} />
         </Route>
       </Routes>
     </Router>
