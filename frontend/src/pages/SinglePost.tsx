406612687e484e13be64691c652426c1427fa03c
Robert Gevorgyan <robert1999.g@gmail.com>
2024-11-13 18:37:54 +0400

    Add page for single post

---
+++ frontend/src/pages/SinglePost.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+import CommentRow from "../components/CommentRow";
+import { fetcher } from "../utils/axios";
+import { endpoints } from "../utils/endpoints";
+import { TComment } from "../types";
+
+export default function SinglePost() {
+  const { id = "" } = useParams();
+
+  // Maybe show toast on error
+  const { data: post, isLoading } = useQuery({
+    queryKey: ["post", id],
+    enabled: Boolean(id),
+    queryFn: async () => fetcher(endpoints.posts.byId(id || "")),
+  });
+
+  const { data: comments, isLoading: isLoadingComments } = useQuery({
+    queryKey: ["comments", id],
+    enabled: Boolean(id),
+    queryFn: async () => fetcher(endpoints.comments.list(id, 0)),
+  });
+
+  if (isLoading) return <span>Loading...</span>;
+
+  return (
+    <div>
+      <h3>{post.title}</h3>
+      <p className="w-full align-right">
+        Created By: {post?.created_by?.email}
+      </p>
+      <h5>{post.content}</h5>
+      <hr />
+      <h4>Comments</h4>
+      <div>
+        {comments.map((comment: TComment) => (
+          <CommentRow {...comment} />
+        ))}
+      </div>
+    </div>
+  );
+}
